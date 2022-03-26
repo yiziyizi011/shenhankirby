73b8aaa8a7d4c59aebb0b22a9d91dc84dda3c6fd
yuekaim <yuekaim@gmail.com>
2022-03-26 01:34:11 +0100

    new fields

--- site/templates/about.php
+++ site/templates/about.php
@@ -3,6 +3,9 @@
 
 <div class="about">
   <?php echo $page->about()->kirbytext() ?>
+  <?php echo $page->education()->kirbytext() ?>
+  <?php echo $page->solo()->kirbytext() ?>
+  <?php echo $page->group()->kirbytext() ?>
 </div>
 
 
