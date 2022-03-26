4e592f37709f9be105f8102c78b4b5e6f4a8c865
yiziyizi011 <82420030+yiziyizi011@users.noreply.github.com>
2022-03-26 01:38:28 +0100

    about

--- site/templates/about.php
+++ site/templates/about.php
@@ -1,11 +1,20 @@
 <?php snippet('header') ?>
 <?php snippet('menu') ?>
 
-<div class="about">
-  <?php echo $page->about()->kirbytext() ?>
-  <?php echo $page->education()->kirbytext() ?>
-  <?php echo $page->solo()->kirbytext() ?>
-  <?php echo $page->group()->kirbytext() ?>
+<div class="textwrapper">
+  <div class="about">
+    <?php echo $page->about()->kirbytext() ?>
+  </div>
+  <div class="education">
+    <?php echo $page->education()->kirbytext() ?>
+  </div>
+  <div class="solo">
+    <?php echo $page->solo()->kirbytext() ?>
+  </div>
+  <div class="group">
+    <?php echo $page->group()->kirbytext() ?>
+  </div>
+
 </div>
 
 
